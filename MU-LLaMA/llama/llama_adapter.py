09eebe2ed4931153dd8ec7ef49484ff68a37659d
Sahil <sahil@sahils-mbp.lan>
2024-11-28 13:59:16 -0800

    ...

--- MU-LLaMA/llama/llama_adapter.py
+++ MU-LLaMA/llama/llama_adapter.py
@@ -68,9 +68,9 @@ class LLaMA_adapter(nn.Module):
         #model_args: ModelArgs = ModelArgs(
         #    max_seq_len=8192, max_batch_size=1, w_bias=bias_lora, w_lora=bias_lora,
         #    **params)  # max_batch_size only affects inference
-        # reducing max_seq_len to 2048; hopefully this is okay for the audio
+        # reducing max_seq_len; hopefully this is okay for the audio
         model_args: ModelArgs = ModelArgs(
-                max_seq_len=2048, max_batch_size=1, w_bias=bias_lora, w_lora=bias_lora,
+                max_seq_len=1024, max_batch_size=1, w_bias=bias_lora, w_lora=bias_lora,
                 **params)  # max_batch_size only affects inference
         print(f"model args: {model_args}")
         model_args.vocab_size = self.tokenizer.n_words
